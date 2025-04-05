8940ab973c49bdc8dc216cccf6fbbbbb8db0b1f8
Dennisse-Campos <dennisse.campos@lc.cuny.edu>
2025-04-04 20:40:14 -0400

    Update payment_process.php

--- payment_process.php
+++ payment_process.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$dbname = 'HotelDB';
+$dbname = 'Hotel_DB';
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
